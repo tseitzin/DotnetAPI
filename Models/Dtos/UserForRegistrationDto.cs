cb47a42c8371f013300d046fe1da8a31acf74b97
tseitzinger <30446692+tseitzin@users.noreply.github.com>
2024-05-29 20:54:07 -0400

    updated auth

--- Models/Dtos/UserForRegistrationDto.cs
+++ Models/Dtos/UserForRegistrationDto.cs
@@ -5,9 +5,25 @@ namespace DotNetAPI.Models.Dtos
         public string Email { get; set; }
         public string Password { get; set; }
         public string PasswordConfirm { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
 
+        
         public UserForRegistrationDto()
         {
+            if (FirstName == null)
+            {
+                FirstName = "";
+            }
+            if (LastName == null)
+            {
+                LastName = "";
+            }
+            if (Gender == null)
+            {
+                Gender = "";
+            }
             if(Email == null)
             {
                 Email = "";
